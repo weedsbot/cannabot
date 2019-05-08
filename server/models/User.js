b42f440aae715581035af2394c82f416113d4abf
ernestogl88 <ernestogomezlopez@gmail.com>
2019-05-08 18:00:16 +0200

    Change user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  facebookId: { type: String, required: true },
+  image_url: { type: String },
   username: { type: String, required: true },
+  password: {type: String, required: true},
   strains: [{ type: Schema.Types.ObjectId, ref: "Strain" }],
   rol: {enum : ['admin','partner']}
 });
